268a2581fb5813d04f46074d6bc8b6700db85e1c
msunaidi <msunaidi@thiqah.sa>
2023-01-31 15:23:25 +0300

    added foneparts

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ function log() {
   const email = document.getElementById("email");
   const phone = document.getElementById("phone");
   const dropdown = document.getElementById("dropdown");
-  console.log(
-    "Name: " +
-      username.value +
-      "| Email: " +
-      email.value +
-      "| Phone Number:" +
-      phone.value +
-      "| How did you find us:" +
-      dropdown.value
-  );
-}
 
-// document.getElementById("btn").addEventListener("click", log);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:4000/submit");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      console.log(response);
+    }
+  };
+
+  const data = JSON.stringify({
+    name: username.value,
+    email: email.value,
+    phone: phone.value,
+    foundMethod: dropdown.value,
+  });
+
+  xhr.send(data);
+}
